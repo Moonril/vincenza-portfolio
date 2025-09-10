78f407d8ed7fc8f1ab7410ef54a228001db6a371
Vincenza <vinni2690@hotmail.com>
2025-09-10 12:31:02 +0200

    about section responsive

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-scroll"
 
 const AboutSection = function () {
     return (
-        <section className="min-h-screen flex flex-col justify-center p-20 items-center">
+        <section className="min-h-screen flex flex-col justify-center p-3 md:p-20 items-center">
         {/* <section className="bg-[url('https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg')] min-h-screen flex flex-col justify-center p-20"> */}
-            <div className=" rounded-2xl w-3xl p-3 text-white font-bold">
-                <h1 className="text-5xl">Hi! I'm <span className="text-transparent bg-clip-text font-extrabold bg-gradient-to-r from-[#C8005B] to-red-500">Vincenza</span></h1>
-                <p>About a year and half ago I started helping some friends modify some .lua files, they were struggling to understand the code but it seemed very easy to me. After a while I decided to meddle with some HTML, CSS and JS for fun. and realised that this could be more than a little hobby. After a period of solo studying I decided to attend a bootcamp to better understand where this path could take me, this is the reason why I chose a Full-Stack course, I wanted to know more about the whole ordeal. Ultimately I did not betray my creative nature and stayed true to my affection for Front End development</p>
+            <div className="lg:w-3xl p-3 text-white font-bold">
+                <h1 className="text-5xl pb-3">Hi! I'm <span className="text-transparent bg-clip-text font-extrabold bg-gradient-to-r from-[#C8005B] to-red-500">Vincenza</span>,</h1>
+                <p className="text-justify">About a year and half ago I started helping some friends modify some .lua files. While they found the code challenging, I quickly realized it came naturally to me. After a while I began experimenting some HTML, CSS and Javascript for fun, and I realised that this could be more than a little hobby. After a period of solo studying I decided to attend a bootcamp to better understand where this path could take me, this is the reason why I chose a Full-Stack course, I wanted to know more about the whole ordeal. Ultimately I did not betray my creative nature and stayed true to my affection for Front End development.</p>
             </div>
             <Link to="projects" smooth={true} className="cursor-pointer hover:translate-y-2 transition duration-500 ease-in-out">
                 <img src="https://img.icons8.com/?size=100&id=37218&format=png&color=ffffff" alt="arrow-down-icon" width={50} />
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ const Sidebar = function () {
     
 
     return (
-        <section className="bg-transparent fixed left-0 top-0 bottom-0 flex flex-col justify-center p-3 z-10 gap-2">
+        <section className="bg-transparent fixed left-0 top-0 bottom-0 flex flex-col justify-start md:justify-center p-3 z-10 gap-2">
             <Link to='about' smooth={true} className='group relative flex items-center p-1 cursor-pointer bg-opacity-10'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-red-500">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205 3 1m1.5.5-1.5-.5M6.75 7.364V3h-3v18m3-13.636 10.5-3.819" />
