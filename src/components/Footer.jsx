371f8a61a7cd3a2d512f79e7a085bad61ca93cd2
Vincenza <vinni2690@hotmail.com>
2025-10-23 17:22:11 +0200

    fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = function () {
     return (
-        <section className="bg-black text-white text-center py-2">Vincenza Fumarulo &middot; {new Date().getFullYear()} &middot; <a href="https://github.com/moonril/">Git</a> &middot; <a href="https://www.linkedin.com/in/vincenza-fumarulo/">Linkedin</a> </section>
+        <section className="bg-black text-white text-center py-2">Vincenza Fumarulo &middot; {new Date().getFullYear()} &middot; <a href="https://github.com/moonril/" className="hover:underline">Git</a> &middot; <a href="https://www.linkedin.com/in/vincenza-fumarulo/" className="hover:underline">Linkedin</a> </section>
     )
 }
 
