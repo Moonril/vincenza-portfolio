43f4a042fdda1d0ceae1f63907cb30b1326a3d9d
Vincenza <vinni2690@hotmail.com>
2026-06-10 11:57:30 +0200

    section order

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -8,12 +8,23 @@ const Banner = function () {
         day: "numeric",
     })
 
+    const seasons = {
+        "spring": "spring.png",
+        "summer": "beach-5.png",
+        "fall": "yeallow-leaves.jpg",
+        "winter": "winter.png"
+
+    }
+
+    
+
     return(
         <section className="text-white font-medium flex flex-row justify-end items-center p-3 gap-2">
             {/* <p>{fullDate}</p> */}
             <div className="group relative">
-                <div className="rounded-full aspect-square w-15 border-3 text-orange-800 overflow-hidden">
-                    <img src="yellow-leaves.jpg" alt="" className="w-full h-full object-cover scale-150" />
+                <div className="rounded-full aspect-square w-15 border-3 text-green-800 overflow-hidden">
+                    {/* <img src="yellow-leaves.jpg" alt="" className="w-full h-full object-cover scale-150" /> */}
+                    <img src="beach-5.png" alt="" className="w-full h-full object-cover" />
                 </div>
                 
                 <p className="absolute right-full top-1/3 mr-2 whitespace-nowrap opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-white">{fullDate}</p>
