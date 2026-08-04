94ae4eaad502dd118e2816cd6aac3a6480f76a40
Vincenza <vinni2690@hotmail.com>
2026-08-04 10:49:27 +0200

    summer spring

--- src/utils/seasons.js
+++ src/utils/seasons.js
@@ -1,41 +1,64 @@
 export const SEASONS = {
   winter: {
-    months: [11, 0, 1], // Dec, Jan, Feb 
-    particleColors: ['#a5c9ff', '#e0f0ff', '#ffffff'],
-    nameColors: ['#a5c9ff', '#e0f0ff'], 
-    sideMenuColor: ['#a5c9ff', '#e0f0ff'], //
-    cardColor: '',
-    activeCardColor: '',
-    accentColors: ['#a5c9ff', '#e0f0ff'],
-  },
-  spring: {
-    months: [2, 3, 4],
-    particleColors: ['#ffb7d5', '#b6f2b6', '#fff59d'],
-    nameColors: ['#a5c9ff', '#e0f0ff'],
-    sideMenuColor: ['#ffb7d5', '#ffb7d5'], //
-    cardColor: '',
-    activeCardColor: '',
-    accentColors: ['#a5c9ff', '#e0f0ff'],
-  },
-  summer: {
-    months: [5, 6, 7],
- 
+    months: [11, 0, 7], 
+
     bannerImage: 'beach-5.png', 
 
-    particleColors: ['#f2233a', '#f99768'], 
+    particleColors: ['#0aaec4', '#0a2fc4'], 
 
-    nameColors: ['#C8005B', '#ef4444'],
-    titlesColor: '#ea580c', 
-    textColor: '#f97316', 
+    nameColors: ['#0a2fc4', '#0aaec4'],
+    titlesColor: '#0aaec4', 
+    textColor: '#0aaec4', 
 
-    sideMenuColor: ['#ef4444', '#fecaca'], 
+    sideMenuColor: ['#0a2fc4', '#0aaec4'], 
 
-    cardColor: '#854d0e', 
-    hoverCardColor: '#991b1b',  
+    cardColor: '#0aaec4', 
+    hoverCardColor: '#0a2fc4',  
     highlightColor: '#86efac',
     hoverHighlightColor: '#22d3ee',
 
-    skillColor: '#7c2d12',
+    skillColor: '#0aaec4',
+  },
+  spring: {
+    months: [2, 3, 4],
+    
+    bannerImage: 'beach-5.png', 
+
+    particleColors: ['#de37a1', '#610dd6'], 
+
+    nameColors: ['#d60d50', '#d60d72'],
+    titlesColor: '#c40a48', 
+    textColor: '#c40a48', 
+
+    sideMenuColor: ['#610dd6', '#de37a1'], 
+
+    cardColor: '#c40a48', 
+    hoverCardColor: '#610dd6',  
+    highlightColor: '#86efac',
+    hoverHighlightColor: '#22d3ee',
+
+    skillColor: '#d60d72',
+    
+  },
+  summer: {
+    months: [5, 6, 8],
+ 
+    bannerImage: 'beach-5.png', 
+
+    particleColors: ['#42f5aa', '#04631a'], 
+
+    nameColors: ['#04631a', '#42f5aa'],
+    titlesColor: '#04631a', 
+    textColor: '#09d99d', 
+
+    sideMenuColor: ['#04631a', '#42f5aa'], 
+
+    cardColor: '#094ed9', 
+    hoverCardColor: '#09d99d',  
+    highlightColor: '#86efac',
+    hoverHighlightColor: '#22d3ee',
+
+    skillColor: '#42f5aa',
 },
 autumn: {
     months: [8, 9, 10],
